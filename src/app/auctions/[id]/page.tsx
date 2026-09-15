5e2adb9de11021490ced6252693db86199ee058a
Maria Traut <mia.gotcha@proton.me>
2026-09-15 12:50:54 +0200

    add styling components and dark mode toggle

--- src/app/auctions/[id]/page.tsx
+++ src/app/auctions/[id]/page.tsx
@@ -3,13 +3,28 @@ import { getAuctionById } from "@/lib/auctions.service";
 import { getOffersByAuctionId } from "@/lib/offers.service";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  TableHead,
+  TableHeader,
+} from "@/components/ui/table";
 
 export default async function AuctionDetailPage({
   params,
 }: PageProps<"/auctions/[id]">) {
   const { id } = await params;
   const auctionData = await getAuctionById(id);
-  console.log("auctionData:", auctionData);
+
   if (auctionData.statusCode === 404) {
     notFound();
   }
@@ -20,43 +35,52 @@ export default async function AuctionDetailPage({
     <>
       <Link href="/auctions">&#x2B60; Auctions List</Link>
       {auctionData && (
-        <>
-          {auctionData.title && <h2>{auctionData.title}</h2>}
-          {auctionData.description && <p>{auctionData.description}</p>}
-          {auctionData.endDate && (
-            <p>
-              Auction end: <LocaleDateString date={auctionData.endDate} />
-            </p>
-          )}
-          {auctionData.currentPrice && (
-            <p>Current price: {auctionData.currentPrice}</p>
-          )}
-          <h2 className="text-xl mt-3">Bid History</h2>
-          {offersData ? (
-            <table>
-              <thead>
-                <tr>
-                  <th>Price</th>
-                  <th>Bidder</th>
-                  <th>Time</th>
-                </tr>
-              </thead>
-              <tbody>
-                {offersData.map((offer) => (
-                  <tr key={offer.id}>
-                    <td>{offer.offerPrice}</td>
-                    <td>{offer.bidder.username}</td>
-                    <td>
-                      <LocaleDateString date={offer.createdAt} />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          ) : (
-            "No offers yet."
-          )}
-        </>
+        <Card>
+          <CardHeader>
+            <CardTitle>
+              {auctionData.title && <h2>{auctionData.title}</h2>}
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            {" "}
+            <CardDescription>
+              {auctionData.description && <p>{auctionData.description}</p>}
+            </CardDescription>
+            {auctionData.endDate && (
+              <p>
+                Auction end: <LocaleDateString date={auctionData.endDate} />
+              </p>
+            )}
+            {auctionData.currentPrice && (
+              <p>Current price: {auctionData.currentPrice} $</p>
+            )}
+            <h2 className="text-xl mt-5">Bid History</h2>
+            {offersData ? (
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Price</TableHead>
+                    <TableHead>Bidder</TableHead>
+                    <TableHead>Time</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {offersData.map((offer) => (
+                    <TableRow key={offer.id}>
+                      <TableCell>{offer.offerPrice} $</TableCell>
+                      <TableCell>{offer.bidder.username}</TableCell>
+                      <TableCell>
+                        <LocaleDateString date={offer.createdAt} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            ) : (
+              "No offers yet."
+            )}
+          </CardContent>
+        </Card>
       )}
     </>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/ModeToggle";
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,16 +30,31 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-mono",
+        jetbrainsMono.variable,
+      )}
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
-        <header className="mb-5 p-3">
-          <h1 className="text-3xl">
-            <Link href="/">nextBay</Link>
-          </h1>
-        </header>
-        <main className="p-3">{children}</main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header className="mb-5 p-3 flex flex-row justify-between">
+            <h1 className="text-3xl">
+              <Link href="/">nextBay</Link>
+            </h1>
+            <ModeToggle />
+          </header>
+          <main className="p-3">{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
